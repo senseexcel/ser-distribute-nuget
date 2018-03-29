17338760a62cdc4f295fd4cf1e03e5b64df6203f
Martin Berthold <martin.berthold@akquinet.de>
2018-03-29 11:10:22 +0200

    First Version 1.0.0

---
+++ src/Distribute.cs
@@ -0,0 +1,136 @@
+﻿namespace SerDistribute
+{
+    #region Usings
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
+    using NLog;
+    using SerApi;
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    #endregion
+
+    public class Distribute
+    {
+        #region Logger
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        #endregion
+
+        private T GetSettings<T>(JToken json, bool typeOnly = false) where T : ISettings, new()
+        {
+            try
+            {
+                if (typeOnly)
+                {
+                    var jProperty = json as JProperty;
+                    switch (jProperty?.Name)
+                    {
+                        case "mail":
+                            return new T() { Type = SettingsType.MAIL };
+                        case "hub":
+                            return new T() { Type = SettingsType.HUB };
+                        case "file":
+                            return new T() { Type = SettingsType.FILE };
+                    }
+                }
+
+                return JsonConvert.DeserializeObject<T>(json.First().ToString());
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex);
+                return default(T);
+            }
+        }
+
+        public string Run(string resultFolder, bool onDemand = false)
+        {
+            try
+            {
+                var execute = new ExecuteManager();
+                logger.Info("Read json result files...");
+                string[] jsonPaths = Directory.GetFiles(resultFolder, "*.json", SearchOption.TopDirectoryOnly);
+                foreach (var jsonPath in jsonPaths)
+                {
+                    if (!File.Exists(jsonPath))
+                    {
+                        logger.Error($"The json result path \"{jsonPath}\" not found.");
+                        continue;
+                    }
+
+                    var json = File.ReadAllText(jsonPath);
+                    var result = JsonConvert.DeserializeObject<JobResult>(json,
+                                 new JsonSerializerSettings() { TypeNameHandling = TypeNameHandling.Auto });
+
+                    if (result.Status != TaskStatusInfo.SUCCESS)
+                    {
+                        logger.Warn($"The result \"{result.Status}\" of the report {jsonPath} is not correct. The report is ignored.");
+                        continue;
+                    }
+
+                    var mailList = new List<MailSettings>();
+                    var uploadTasks = new List<Task>();
+                    foreach (var report in result.Reports)
+                    {
+                        var locations = report.Evaluate.Children();
+                        foreach (var location in locations)
+                        {
+                            var settings = GetSettings<FileSettings>(location, true);
+                            switch (settings.Type)
+                            {
+                                case SettingsType.FILE:
+                                    //copy reports
+                                    logger.Info("Check - Copy Files...");
+                                    var fileSettings = GetSettings<FileSettings>(location);
+                                    execute.CopyFile(fileSettings, report.Paths, report.Name);
+                                    break;
+                                case SettingsType.HUB:
+                                    //upload to hub
+                                    logger.Info("Check - Upload to hub...");
+                                    var hubSettings = GetSettings<HubSettings>(location);
+                                    var task = execute.UploadToHub(hubSettings, report.Paths, report.Name, onDemand);
+                                    if (task != null)
+                                        uploadTasks.Add(task);
+                                    break;
+                                case SettingsType.MAIL:
+                                    //cache mail infos
+                                    logger.Info("Check - Cache Mail...");
+                                    var mailSettings = GetSettings<MailSettings>(location);
+                                    if (mailSettings.Active)
+                                    {
+                                        mailSettings.Paths = report.Paths;
+                                        mailSettings.ReportName = report.Name;
+                                        mailList.Add(mailSettings);
+                                    }
+                                    break;
+                                default:
+                                    logger.Warn($"The delivery type of json {location} is unknown.");
+                                    break;
+                            }
+                        }
+                    }
+
+                    //wait for all upload tasks
+                    Task.WaitAll(uploadTasks.ToArray());
+
+                    //Send Mail
+                    logger.Info("Check - Send Mails...");
+                    execute.SendMails(mailList);
+                }
+
+                if (onDemand)
+                    return execute.OnDemandDownloadLink;
+                else
+                    return "OK";
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex);
+                return null;
+            }
+        }
+    }
+}
---
+++ src/ExecuteManager.cs
@@ -0,0 +1,387 @@
+﻿namespace SerDistribute
+{
+    #region Usings
+    using System.Collections.Generic;
+    using System.IO;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
+    using NLog;
+    using System;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text;
+    using System.Threading.Tasks;
+    using SerApi;
+    using System.Net;
+    using Q2gHelperQrs;
+    using System.Net.Mail;
+    using System.Net.Http;
+    #endregion
+
+    public class ExecuteManager
+    {
+        #region Logger
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        #endregion
+
+        #region Properties
+        public string OnDemandDownloadLink { get; set; }
+        #endregion
+
+        #region Private Methods
+        private string GetHost(SerConnection connection, bool withSheme = true, bool withProxy = true)
+        {
+            var url = $"{connection.ConnectUri}";
+            if (withProxy == false)
+                return url;
+            if (!String.IsNullOrEmpty(connection.VirtualProxyPath))
+                url += $"/{connection.VirtualProxyPath}";
+
+            if (!withSheme)
+            {
+                var uri = new Uri(url);
+                return url.Replace($"{uri.Scheme.ToString()}://", "");
+            }
+
+            return url;
+        }
+
+        private List<JToken> GetConnections(string host, string appId, Cookie cookie)
+        {
+            try
+            {
+                var results = new List<string>();
+                var qlikWebSocket = new QlikWebSocket(host, cookie);
+                var isOpen = qlikWebSocket.OpenSocket();
+                var response = qlikWebSocket.OpenDoc(appId);
+                var handle = response["result"]["qReturn"]["qHandle"].ToString();
+                response = qlikWebSocket.GetConnections(handle);
+                return response["result"]["qConnections"].ToList();
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "No qlik lib connections found.");
+                return null;
+            }
+        }
+
+        private string NormalizeLibPath(string path, SerConnection settings)
+        {
+            var workDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var cookie = new Cookie(settings.Credentials.Key, settings.Credentials.Value);
+            var connectUri = GetHost(settings, false);
+            var libUri = new Uri(path);
+            var connections = GetConnections(connectUri, settings.App, cookie);
+            if (connections != null)
+            {
+                var libResult = connections.FirstOrDefault(n => n["qName"].ToString().ToLowerInvariant() == libUri.Host);
+                var libPath = libResult["qConnectionString"].ToString();
+                return Path.Combine(libPath, libUri.LocalPath.Replace("/", "\\").Trim().Trim('\\'));
+            }
+
+            return null;
+        }
+
+        private HubInfo GetSharedContentFromUser(QlikQrsHub hub, string name, DomainUser hubUser)
+        {
+            var hubRequest = new HubSelectRequest()
+            {
+                Filter = HubSelectRequest.GetNameFilter(name),
+            };
+            var sharedContentInfos = hub.GetSharedContentAsync(hubRequest)?.Result;
+            if (sharedContentInfos == null)
+                return null;
+
+            if (hubUser == null)
+                return sharedContentInfos.FirstOrDefault() ?? null;
+
+            foreach (var sharedContent in sharedContentInfos)
+            {
+                if(sharedContent.Owner.UserId == hubUser.UserId && 
+                   sharedContent.Owner.UserDirectory == hubUser.UserDirectory)
+                {
+                    return sharedContent;
+                }
+            }
+
+            return null;
+        }
+
+        #endregion
+
+        public void CopyFile(FileSettings settings, List<string> paths, string reportName)
+        {
+            try
+            {
+                //Copy Files
+                if (settings.Active == false)
+                    return;
+
+                var targetPath = settings.TargetPath.ToLowerInvariant().Trim();
+                if (!targetPath.StartsWith("lib://"))
+                {
+                    logger.Error($"Target value \"{targetPath}\" is not a lib:// folder.");
+                    return;
+                }
+
+                targetPath = NormalizeLibPath(targetPath, settings.Connection);
+                if (targetPath == null)
+                    throw new Exception("The could not resolved.");
+
+                logger.Info($"Resolve target path: \"{targetPath}\".");
+                Directory.CreateDirectory(targetPath);
+                foreach (var path in paths)
+                {
+                    var targetFile = Path.Combine(targetPath, $"{reportName}");
+                    File.Copy(path, targetFile, settings.Overwrite);
+                    logger.Info($"file {targetFile} was copied");
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "The copying process could not be execute.");
+            }
+        }
+
+        public Task UploadToHub(HubSettings settings, List<string> paths, string reportName, bool ondemandMode)
+        {
+            try
+            {
+                //Upload to Hub
+                if (settings.Active == false)
+                    return null;
+
+                var workDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                var connectUri = new Uri(GetHost(settings.Connection));
+                var hub = new QlikQrsHub(connectUri, new Cookie(settings.Connection.Credentials.Key,
+                                                                settings.Connection.Credentials.Value));
+                foreach (var path in paths)
+                {
+                    var contentName = $"{Path.GetFileNameWithoutExtension(reportName)} ({Path.GetExtension(path).TrimStart('.').ToUpperInvariant()})";
+                    var newPath = Path.Combine(Path.GetDirectoryName(path), reportName);
+                    if (!File.Exists(newPath))
+                        File.Move(path, newPath);
+                    if (settings.Mode == HubMode.OVERRIDE)
+                    {
+                        HubInfo hubInfo = null;
+                        Guid? hubUserId = null;
+                        DomainUser hubUser = null;
+                        if (settings.HubUser != null)
+                        {
+                            hubUser = new DomainUser(settings.HubUser);
+                            var userUri = new Uri($"{connectUri}/qrs/user");
+                            var filter = $"userId eq '{hubUser.UserId}' and userDirectory eq '{hubUser.UserDirectory}'";
+                            var result = hub.SendRequestAsync(userUri, HttpMethod.Get, null, filter).Result;
+                            if (result == null)
+                                throw new Exception($"Qlik user {settings.HubUser} with qrs not found or session not connected.");
+                            var userObject = JArray.Parse(result);
+                            if (userObject.Count != 1)
+                                throw new Exception($"Too many User found. {result}");
+                            hubUserId = new Guid(userObject.First()["id"].ToString());
+                        }
+
+                        var sharedContent = GetSharedContentFromUser(hub, contentName, hubUser);
+                        if (sharedContent == null)
+                        {
+                            var createRequest = new HubCreateRequest()
+                            {
+                                Name = contentName,
+                                Description = "Created by Sense Excel Reporting",
+                                Data = new ContentData()
+                                {
+                                    ContentType = $"application/{Path.GetExtension(newPath).Trim('.')}",
+                                    ExternalPath = Path.GetFileName(newPath),
+                                    FileData = File.ReadAllBytes(newPath),
+                                }
+                            };
+
+                            hubInfo = hub.CreateSharedContentAsync(createRequest).Result;
+                        }
+                        else
+                        {
+                            var updateRequest = new HubUpdateRequest()
+                            {
+                                Info = sharedContent,
+                                Data = new ContentData()
+                                {
+                                    ContentType = $"application/{Path.GetExtension(newPath).Trim('.')}",
+                                    ExternalPath = Path.GetFileName(newPath),
+                                    FileData = File.ReadAllBytes(newPath),
+                                }
+                            };
+
+                            hubInfo = hub.UpdateSharedContentAsync(updateRequest).Result;
+                        }
+
+                        if (ondemandMode)
+                        {
+                            var refpath = hubInfo?.References?.FirstOrDefault()?.ExternalPath ?? null;
+                            OnDemandDownloadLink = $"{GetHost(settings.Connection, true, false)}{refpath}";
+                        }
+
+                        if (hubUserId != null)
+                        {
+                            var newHubInfo = new HubInfo()
+                            {
+                                Id = hubInfo.Id,
+                                Type = "Qlik report",
+                                Owner = new HubOwer()
+                                {
+                                    Id = hubUserId,
+                                    UserId = hubUser.UserId,
+                                    UserDirectory = hubUser.UserDirectory,
+                                    Name = hubUser.UserId,
+                                }
+                            };
+
+                            var changeRequest = new HubUpdateRequest()
+                            {
+                                Info = newHubInfo,
+                            };
+
+                            return hub.UpdateSharedContentAsync(changeRequest);
+                        }
+                    }
+                    else if (settings.Mode == HubMode.DELETE)
+                    {
+                        var hubRequest = new HubSelectRequest()
+                        {
+                            Filter = HubSelectRequest.GetNameFilter(contentName),
+                        };
+                        var sharedContent = hub.GetSharedContentAsync(hubRequest)?.Result?.FirstOrDefault() ?? null;
+                        if (sharedContent != null)
+                        {
+                            var deleteRequest = new HubDeleteRequest()
+                            {
+                                Id = sharedContent.Id.Value,
+                            };
+                            return hub.DeleteSharedContentAsync(deleteRequest);
+                        }
+                    }
+                    else if (settings.Mode == HubMode.DELETEALL)
+                    {
+                        return hub.DeleteAllSharedContentAsync();
+                    }
+                    else
+                    {
+                        throw new Exception($"Unknown hub mode {settings.Mode}");
+                    }
+                }
+
+                return null;
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "The process could not be upload to the hub.");
+                return null;
+            }
+        }
+
+        public void SendMails(List<MailSettings> settingsList)
+        {
+            try
+            {
+                var mailList = new List<EMailReport>();
+                foreach (var mailSettings in settingsList)
+                {
+                    foreach (var path in mailSettings.Paths)
+                    {
+                        var result = mailList.SingleOrDefault(m => m.MailInfo.ToString() == mailSettings.ToString());
+                        if (result == null)
+                        {
+                            var mailReport = new EMailReport(mailSettings, mailSettings.MailServer, mailSettings.ToString());
+                            mailReport.AddReport(path, mailSettings.ReportName);
+                            mailList.Add(mailReport);
+                        }
+                        else
+                        {
+                            result.AddReport(path, mailSettings.ReportName);
+                        }
+                    }
+                }
+
+                //send merged mail infos
+                foreach (var report in mailList)
+                {
+                    var toAddresses = report.Settings.EMail?.To?.Split(';') ?? new string[0];
+                    var ccAddresses = report.Settings.EMail?.Cc?.Split(';') ?? new string[0];
+                    var bccAddresses = report.Settings.EMail?.Bcc?.Split(';') ?? new string[0];
+
+                    var mailMessage = new MailMessage()
+                    {
+                        Subject = report.Settings.Subject,
+                    };
+                    var msgBody = report.Settings.Message;
+                    if (msgBody.Contains("</html>"))
+                        mailMessage.IsBodyHtml = true;
+                    else
+                        mailMessage.Body = msgBody.Replace("{n}", "\r\n");
+
+                    mailMessage.From = new MailAddress(report.ServerSettings.From);
+                    foreach (var attach in report.ReportPaths)
+                        mailMessage.Attachments.Add(attach);
+
+                    foreach (var address in toAddresses)
+                    {
+                        if (!String.IsNullOrEmpty(address))
+                            mailMessage.To.Add(address);
+                    }
+
+                    foreach (var address in ccAddresses)
+                    {
+                        if (!String.IsNullOrEmpty(address))
+                            mailMessage.CC.Add(address);
+                    }
+
+                    foreach (var address in bccAddresses)
+                    {
+                        if (!String.IsNullOrEmpty(address))
+                            mailMessage.Bcc.Add(address);
+                    }
+
+                    var client = new SmtpClient(report.ServerSettings.Host, report.ServerSettings.Port)
+                    {
+                        Credentials = new NetworkCredential(report.ServerSettings.Username, report.ServerSettings.Password),
+                    };
+                    client.Send(mailMessage);
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "The reports could not be sent as mail.");
+            }
+        }
+    }
+
+    public class EMailReport
+    {
+        #region Varibales & Properties
+        public MailSettings Settings { get; private set; }
+        public MailServerSettings ServerSettings { get; private set; }
+        public List<Attachment> ReportPaths { get; private set; }
+        public JToken MailInfo { get; private set; }
+        #endregion
+
+        #region Constructor
+        public EMailReport(MailSettings settings, MailServerSettings serverSettings, JToken mailInfo)
+        {
+            Settings = settings;
+            ServerSettings = serverSettings;
+            MailInfo = mailInfo;
+            ReportPaths = new List<Attachment>();
+        }
+        #endregion
+
+        #region Methods
+        public void AddReport(string reportPath, string name)
+        {
+            var attachment = new Attachment(reportPath)
+            {
+                 Name = $"{name}{Path.GetExtension(reportPath)}",
+            };
+
+            ReportPaths.Add(attachment);
+        }
+        #endregion
+    }
+}
