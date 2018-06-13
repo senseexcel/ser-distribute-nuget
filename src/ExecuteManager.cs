2806a1cf1f747f88378fd90ec548716e0cd0da8f
Martin Berthold <martin.berthold@akquinet.de>
2018-06-13 14:49:09 +0200

    close #25

--- src/ExecuteManager.cs
+++ src/ExecuteManager.cs
@@ -427,7 +427,7 @@
                     {
                         Credentials = new NetworkCredential(report.ServerSettings.Username, report.ServerSettings.Password),
                     };
-                    logger.Debug("send mail package...");
+                    logger.Debug("Send mail package...");
                     client.EnableSsl = report.ServerSettings.UseSsl;
                     client.Send(mailMessage);
                 }
